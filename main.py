8d331ee4dffe4a9f1d60bffc18930e21e43e448a
etlumac <105116609+etlumac@users.noreply.github.com>
2022-05-08 19:32:35 +0300

    Первая конечная версия

--- main.py
+++ main.py
@@ -34,40 +34,40 @@ def func(message):
         btn5 = types.KeyboardButton("Information Security")
         back = types.KeyboardButton("Вернуться в главное меню")
         markup.add(btn1, btn2,btn3,btn4,btn5, back)
-        bot.send_message(message.chat.id, text="Смотри сколько всего интересного", reply_markup=markup)
+        bot.send_message(message.chat.id, text="Смотри сколько всего интересного!", reply_markup=markup)
 
 
     elif (message.text == "Mobile App Developer"):
         markup = types.InlineKeyboardMarkup()
-        btn_my_site = types.InlineKeyboardButton(text='🔗', url='https://habrahabr.ru')
+        btn_my_site = types.InlineKeyboardButton(text='Смотреть 👀', url='https://cerulean-nephew-71d.notion.site/Mobile-App-Developer-7b9d1d43c9934ed18cdb25f26e92e758')
         markup.add(btn_my_site)
         bot.send_message(message.chat.id, "Давай покажу тебе, что нужно этому специалисту? По ссылке ты найдёшь всю информацию, удачи!", reply_markup=markup)
 
 
     elif (message.text == "WEB Developer"):
         markup = types.InlineKeyboardMarkup()
-        btn_my_site = types.InlineKeyboardButton(text='🔗', url='https://habrahabr.ru')
+        btn_my_site = types.InlineKeyboardButton(text='Смотреть 👀', url='https://cerulean-nephew-71d.notion.site/WEB-Developer-75fcd595a4724baa815431c9540da91f')
         markup.add(btn_my_site)
         bot.send_message(message.chat.id, "Давай покажу тебе, что нужно этому специалисту? По ссылке ты найдёшь всю информацию, удачи!", reply_markup=markup)
 
 
     elif (message.text == "Database Administrator"):
         markup = types.InlineKeyboardMarkup()
-        btn_my_site = types.InlineKeyboardButton(text='🔗', url='https://habrahabr.ru')
+        btn_my_site = types.InlineKeyboardButton(text='Смотреть 👀', url='https://cerulean-nephew-71d.notion.site/Database-Administrator-1e311adc8e3f4f549f0127443892f3fa')
         markup.add(btn_my_site)
         bot.send_message(message.chat.id, "Давай покажу тебе, что нужно этому специалисту? По ссылке ты найдёшь всю информацию, удачи!", reply_markup=markup)
 
 
     elif (message.text == "Machine learning"):
         markup = types.InlineKeyboardMarkup()
-        btn_my_site = types.InlineKeyboardButton(text='🔗', url='https://habrahabr.ru')
+        btn_my_site = types.InlineKeyboardButton(text='Смотреть 👀', url='https://cerulean-nephew-71d.notion.site/12c89284ae4f4dddb0d0ce007a1e87b6')
         markup.add(btn_my_site)
         bot.send_message(message.chat.id, "Давай покажу тебе, что нужно этому специалисту? По ссылке ты найдёшь всю информацию, удачи!", reply_markup=markup)
 
 
     elif message.text == "Information Security":
         markup = types.InlineKeyboardMarkup()
-        btn_my_site = types.InlineKeyboardButton(text='🔗', url='https://habrahabr.ru')
+        btn_my_site = types.InlineKeyboardButton(text='Смотреть 👀', url='https://cerulean-nephew-71d.notion.site/7055119020a142a1ba66e76c1a30beb0')
         markup.add(btn_my_site)
         bot.send_message(message.chat.id, "Давай покажу тебе, что нужно этому специалисту? По ссылке ты найдёшь всю информацию, удачи!", reply_markup=markup)
 
